ebf0fcd018d17d716b817badce69fdc6b62e15f0
Juan jose <juanjozapatarendon@gmail.com>
2025-07-30 15:27:27 -0500

    aplicacion terminada

--- 1_repaso_conceptos/index.js
+++ 1_repaso_conceptos/index.js
@@ -1,4 +1,4 @@
-const imagen = document.querySelector('.img1');
+/* const imagen = document.querySelector('.img1');
 const boton1 = document.querySelector('.btn1');
 const boton2 = document.querySelector('.btn2');
 const boton3 = document.querySelector('.btn3');
@@ -15,6 +15,44 @@ boton2.addEventListener('click', () => {
 
 
 boton3.addEventListener('click', () => {
-  imagen.style.transform = 'scale(1.5) rotate(20deg)';
+  imagen.style.transform = 'scale(1.2) rotate(20deg)';
 });
+ */
+
+const pantalla = document.querySelector('.img1');
+const btn1 = document.querySelector('#btn1');
+const btn2 = document.querySelector('#btn2');
+const btn3 = document.querySelector('#btn3');
+
+//. funciones y eventos
+
+function blancoynegro (){
+  console.log("imagen modificada")
+  pantalla.style.filter = "grayscale(100%)"
+
+}
+
+function desenfoque (){
+  console.log("imagen desenfocada")
+  pantalla.style.filter = "blur(5px)"
+}
+
+function rotacion (){ 
+  console.log("imagen rotacion")
+  pantalla.style.transform = "scale(1.2) rotate(20deg)"
+}
+
+  /* btn1.style.backgroundColor = "blueviolet"
+  btn1.style.color = "white"
+  btn1.style.height = "200px"
+  btn1.style.borderRadius = "50%"
+} */
+
+
+
+btn1.addEventListener("click", blancoynegro )
+btn2.addEventListener("click", desenfoque )
+btn3.addEventListener("click", rotacion )
+
+
 
