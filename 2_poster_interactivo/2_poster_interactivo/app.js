3022381c1f44abeadc4e9e334bd7e29776078b09
Juan jose <juanjozapatarendon@gmail.com>
2025-08-05 15:01:50 -0500

    poster terminado

--- 2_poster_interactivo/2_poster_interactivo/app.js
+++ 2_poster_interactivo/2_poster_interactivo/app.js
@@ -1,16 +1,31 @@
 
 const monedas = document.querySelectorAll('.coin');
-const corazon = document.querySelector('.corazon');
+const corazones = document.querySelectorAll('.corazon');
 const contadormonedas = document.querySelector('#contador-monedas');
 const contadorCorazones = document.querySelector('#contador-corazones');
+let totalmonedas = 0
+let totalcorazones = 0;
 
-console.log(monedas,corazon,contadormonedas,contadorCorazones );
+console.log(monedas, corazones, contadormonedas, contadorCorazones);
 
-monedas.forEach( function(moneda){
-  
-    moneda.addEventListener( 'click', function () {
+monedas.forEach(function (moneda) {
+
+    moneda.addEventListener('click', function () {
         moneda.classList.add('saltar');
-        contadormonedas.textContent = parseInt(contadormonedas.textContent) + 1;
+        totalmonedas++
+        contadormonedas.textContent = totalmonedas
+    })
+
+})
+
+
+
+
+corazones.forEach(function (corazon) {
+    corazon.addEventListener('click', function () {
+        corazon.classList.add('saltar');
+        totalcorazones++
+        contadorCorazones.textContent = totalcorazones
     })
 
 })
