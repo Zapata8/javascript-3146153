5204b8b37d9ddc933d4793f6057f5156fd3b5725
Juan jose <juanjozapatarendon@gmail.com>
2025-08-14 15:32:23 -0500

    audio y video parte 1

---
+++ 4_audio_video_js/app.js
@@ -0,0 +1,8 @@
+const video = document.querySelector('#video');
+const playVideo = document.querySelector('#play-video');
+const stopVideo = document.querySelector('#stop-video');
+const stopImg = document.querySelector('#play-stop-img1');
+const stopimg2 = document.querySelector('#play-stop-img2');
+
+
+console.log(video, playVideo, stopVideo, stopImg, stopimg2);
