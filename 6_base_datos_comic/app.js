78b201286d378f3a69d6305c4b9e32683ccfeb0e
Juan jose <juanjozapatarendon@gmail.com>
2025-09-11 16:21:48 -0500

    Add files via upload
    pagina terminada juan sebastian villamil-Juan Jose Zapata

--- 6_base_datos_comic/app.js
+++ 6_base_datos_comic/app.js
@@ -1,19 +1,51 @@
 const tituloComic =document.querySelector(".titulo-comic");
-const descripcionComic = document.querySelector(".descripcion-comic");
+const descripcion = document.querySelector(".descripcion");
 const imgComic = document.querySelector(".img-comic");
-const listaCaps = document.querySelector(".lista-caps");
+const grid = document.querySelector(".grid");
 
 
 tituloComic.textContent = comic.nombreComic;
-descripcionComic.textContent = comic.descripcion;
+descripcion.textContent = comic.descripcion;
 
 
 comic.capítulos.forEach(cap => {
-    const li = document.createElement("li");
-    li.innerHTML = ` 
-     <p>${cap.nombreCap}</p>
+    const div = document.createElement("div");
+    div.classList.add("card");  
+    div.innerHTML = ` 
     <img src="${cap.imgPortada}"width="150 alt="">
 
+     <p class="card-titulo">${cap.nombreCap}</p>
+    <p class="card-descripcion">${cap.descripcion}</p>
+        
+    
+
 `
-listaCaps.appendChild(li);
+grid.appendChild(div);
 });
+
+
+
+/* 
+
+console.log(comic.nombreComic); // "Cuando La Tierra Calla"
+
+const titulo = document.querySelector('.titulo-comic h2');
+titulo.textContent = comic.nombreComic;
+
+const descripcion = document.querySelector('.descripcion p');
+descripcion.textContent = comic.descripcion;
+
+const episodesContainer = document.querySelector('.grid');
+
+comic.capítulos.forEach((cap, index) => {
+  const card = document.createElement('div');
+  card.classList.add('.card');
+
+  card.innerHTML = `
+    <img src="${cap.imgPortada}" alt="Episode ${index + 1}">
+    <h3 class="num-epi">Episode ${index + 1}</h3>
+    <h3 class="name-cap">${cap.nombreCap}</h3>
+  `;
+
+  episodesContainer.appendChild(card);
+}); */
--- 6_base_datos_comic/bad.js
+++ 6_base_datos_comic/bad.js
@@ -2,15 +2,15 @@ const comic = {
 
 "nombreComic": "Libralos del mal",
 "categoria": "apocalipsis",
-"cantidadCap": 4,
-"descripcion": " la llegada de cristo esta presente y la vida de kael cabia repentinamete, mostrando el punto de vista de los malditos",
-"personajes": ["pepa", "lucy", "mario", "pastor"],
+"cantidadCap": 5    ,
+"descripcion": " La llegada de cristo esta presente y la vida de kael cabia repentinamete, mostrando el punto de vista de los malditos",
+"personajes": ["Kael", "Sandy", "Kyle", "Pastor", "Satanas"],
 "bgBanner": "./assets/Captura de pantalla 2025-09-04 153516.png",
 "capítulos":[
 
 {
 id: 1,
-nombreCap: "libranos del mal, amen.",
+nombreCap: "Libranos del mal, amen.",
 imgPortada: "./assets/Captura de pantalla 2025-09-04 152708.png",
 video: "video/cap1.mp4",
 descripcion: "kael, su esposa sandy y su hijo kael, suelen ir a la iglesia y evidentemente se hacen ver como una familia religiosa y creyente del cristianismo",
@@ -19,10 +19,10 @@ personajes: ["kael", "kyle", "sandy"]
 },
 {
     id: 2,
-nombreCap: "suplicas de ayuda",
+nombreCap: "Suplicas de ayuday",
 imgPortada: "./assets/Captura de pantalla 2025-09-04 152747.png",
 video: "video/cap2.mp4",
-descripcion: "el matriomonio de kael y sandy cada vez esa mas roto, aun asi, los tres integrantes de la familia viven un duelo silencioso  ",
+descripcion: "El matriomonio de kael y sandy cada vez esa mas roto, aun asi, los tres integrantes de la familia viven un duelo silencioso  ",
 personajes: ["kael", "sandy", "kyle"]
 
 },
@@ -30,11 +30,11 @@ personajes: ["kael", "sandy", "kyle"]
 {
     id: 3,
 
-nombreCap: "profecias de fantasia",
+nombreCap: "Profecias de fantasia",
 imgPortada: "./assets/Captura de pantalla 2025-09-04 152818.png",
 video: "video/cap1.mp4",
-descripcion: "suenan trompetas, gritos y relampagos, sandy y kyle han desaparecido y han dejado su ropa tiratda, kael hara todo por encontrarlos y entender que es lo que esta pasando",
-personajes: ["kael", "pastor",]
+descripcion: "Suenan trompetas, gritos y relampagos, sandy y kyle han desaparecido y han dejado su ropa tiratda, kael hara todo por encontrarlos y entender que es lo que esta pasando",
+personajes: ["kael", "Pastor",]
 
 },
 
@@ -43,8 +43,8 @@ personajes: ["kael", "pastor",]
 nombreCap: "No hablar con desconocidos",
 imgPortada: "./assets/Captura de pantalla 2025-09-04 152831.png",
 video: "video/cap1.mp4",
-descripcion: "el miedo invade a kael, aun se niega aceptar el paradero de su familia y solo busca un lugar en donde refugiarse",
-personajes: ["kael", "hombre herido" , "satanas"]
+descripcion: "El miedo invade a kael, aun se niega aceptar el paradero de su familia y solo busca un lugar en donde refugiarse",
+personajes: ["kael", "Hombre herido" , "Satanas"]
 
 },
 
@@ -54,8 +54,8 @@ personajes: ["kael", "hombre herido" , "satanas"]
 nombreCap: "La hora del juicio",
 imgPortada: "./assets/Captura de pantalla 2025-09-04 152845.png",
 video: "video/cap1.mp4",
-descripcion: "el miedo consume a kael, y aunque su destino es dificil de aceptar pierde todo tipo de esperanzas",
-personajes: ["kael", "kyle", "sandy", "satanas"]
+descripcion: "El miedo consume a kael, y aunque su destino es dificil de aceptar pierde todo tipo de esperanzas",
+personajes: ["kael", "kyle", "Sandy", "Satanas"]
 
 },
 ]
